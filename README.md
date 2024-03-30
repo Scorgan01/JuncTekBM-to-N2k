fc56f54c2c722f12625929dcd7709d20b8888962
Ulrich Meine <ulrich.meine@web.de>
2024-03-30 01:29:12 +0100

    v0.9 added RSETT type + battery capacity

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JuncTekBM-to-N2k
 Reads JuncTek KH-F series battery monitor data from RS485 and converts it to NMEA2000.
-Designed for M5Stack Atom Lite ESP32 processor and RS485 Tail + CAN modules.
+Designed for M5Stack Atom Lite ESP32 processor + Tail RS485 and CAN modules.
 
 Creates NMEA 2000 PNGs
 - 127506 (DC detailed status)
--- src/main.ino
+++ src/main.ino
@@ -14,7 +14,7 @@
 
 // Reads battery RS485 messages from Junctek KH1x0F Battery Monitor and forwards them to N2k bus
 // Creates N2k messages: 127508 - battery status, 127506 - DC detailed status, (126996 - product info)
-// Version 0.6, 05/03/2024, Scorgan - Ulrich Meine
+// Version 0.9, 30/03/2024, Scorgan - Ulrich Meine
 
 // Battery monitor message reading and parsing takes ideas from: PlJakobs https://github.com/pljakobs/JuncTek_Batterymonitor/tree/main
 // Uses the great work of Timo Lappalainen and his NMEA 2000 libraries: https://github.com/ttlappalainen
@@ -22,35 +22,32 @@
 // Webserver logging by OER Informatik: https://gitlab.com/oer-informatik/mcu/arduino-esp/-/blob/main/src/loggingWLANWebpage/loggingWLANWebpage.ino
 
 #define DEBUG // Flag to activate logging to serial console (i.e. serial monitor in arduino ide)
-String LOG_LEVEL_NAMES[] = { "OFF", "FATAL", "ERROR", "WARN", "INFO", "DEBUG", "TRACE", "ALL" };
-const int MIN_LOG_LEVEL = 6;
-// #define TEST // no device connected -> provide test data sentence
-// #define WEBLOG  // configure logging via web server
+// #define TEST // provide test data sentence if no device connected
+#define WEBLOG // configure logging via web server
 
 // M5 Atom Lite GPIO settings
 #define ESP32_CAN_TX_PIN GPIO_NUM_22 // set CAN TX port to 22
 #define ESP32_CAN_RX_PIN GPIO_NUM_19 // set CAN RX port to 19
-#define RS485_RX_PIN GPIO_NUM_32     // define the RS485 RX port
-#define RS485_TX_PIN GPIO_NUM_26     // define the RS485 TX port
-#define USE_N2K_CAN 7                // #define for NMEA2000_CAN library for use with ESP32
+#define RS485_RX_PIN GPIO_NUM_32 // define the RS485 RX port
+#define RS485_TX_PIN GPIO_NUM_26 // define the RS485 TX port
+#define USE_N2K_CAN 7 // #define for NMEA2000_CAN library for use with ESP32
 
 #include <Arduino.h>
 // #include <M5stack.h>
-#include <NMEA2000_CAN.h> // This will automatically choose right CAN library and create suitable NMEA2000 object
 #include <N2kMessages.h>
+#include <NMEA2000_CAN.h> // This will automatically choose right CAN library and create suitable NMEA2000 object
 #include <Preferences.h>
 #include <WiFi.h>
 #include <string.h>
 #include <string>
-// #include <Time.h>
-#include <sys/time.h>
+// #include <sys/time.h>
 
 #define BM_ADDRESS 1 // battery monitor device address
+#define BM_MAX_MSG_NO 27 // battery monitor max no. of message fields, incl. checksum
+#define BM_MAX_MSG_SIZE 125 // battery monitor max message size (current max R51 message size is 113 chars)
 const char BM_RBASE_CMD[] = "R00"; // battery monitor read state command
 const char BM_RMSG_CMD[] = "R50"; // battery monitor read measures command
 const char BM_RSETT_CMD[] = "R51"; // battery monitor read settings command
-#define BM_MAX_MSG_NO 27 // battery monitor max no. of message fields, incl. checksum
-#define BM_MAX_MSG_SIZE 125 // battery monitor max message size (current max R51 message size is 113 chars)
 
 #define TempSendOffset 0 // variable name to be changed
 #define SlowDataUpdatePeriod 1000 // Time between CAN Messages sent
@@ -58,6 +55,8 @@ const char BM_RSETT_CMD[] = "R51"; // battery monitor read settings command
 
 HardwareSerial SerRS485(2);
 
+const int MIN_LOG_LEVEL = 6;
+String LOG_LEVEL_NAMES[] = { "OFF", "FATAL", "ERROR", "WARN", "INFO", "DEBUG", "TRACE", "ALL" };
 String setupLogText = "";
 String loopLogText = "";
 
@@ -106,7 +105,7 @@ unsigned long InitNextUpdate(unsigned long Period, unsigned long Offset);
 void SetNextUpdate(unsigned long& NextUpdate, unsigned long Period);
 
 #ifdef WEBLOG
-#include "secrets.h" // Passwords saved in this file to be hidden from versioncontrol and sharing
+#include "secrets.h" // Passwords saved in this file to be hidden from version control and sharing
 #include <WiFiMulti.h>
 
 // WiFi-Settings (if not defined in secrets.h replace your SSID/PW here)
@@ -142,24 +141,12 @@ void setup()
     uint32_t id = 0;
     int i = 0;
 
-    // battery static data -> adjust
-    batteryData.instance = 0x02; // battery instance no. for service battery
-    batteryData.batteryType = N2kDCt_Battery; // type of service battery
-//    batteryData.totalCapacity = 2.2; // capacity of service battery at Ah
-    batteryData.stateOfHealth = 100; // static definition for SoH as 100%
-
-    debugOutput("Starting Programm...", 6, true);
-
-    if (BMGetBatterySettings(BM_ADDRESS)) {
-        debugOutput("Total battery capacity: " + String(batteryData.totalCapacity), 5, true);
-     } else {
-        debugOutput("Error retreiving total battery capacity", 2, true);
-    }
-
 #ifdef DEBUG
     Serial.begin(115200); // Activate debugging via serial monitor
     delay(100);
 #endif
+
+    debugOutput("Starting Programm...", 6, true);
 
 //  Disable WiFi and bluetooth since we don't need it
 #ifndef WEBLOG
@@ -167,8 +154,25 @@ void setup()
 #endif
     btStop();
 
+/*  // Reduce CPU frequency to save power
+    debugOutput("CPU Freq: " + String(getCpuFrequencyMhz()), 6, true);
+    setCpuFrequencyMhz(80);
+    debugOutput("New CPU Freq: " + String(getCpuFrequencyMhz()), 6, true);
+*/
     SerRS485.begin(115200, SERIAL_8N1, RS485_RX_PIN, RS485_TX_PIN); // Initialize RS485 communication
     delay(100);
+
+    // battery static data -> adjust
+    batteryData.instance = 0x02; // battery instance no. for service battery
+    batteryData.batteryType = N2kDCt_Battery; // type of service battery
+    //    batteryData.totalCapacity = 2.2; // capacity of service battery at Ah
+    batteryData.stateOfHealth = 100; // static definition for SoH as 100%
+
+    if (BMGetBatterySettings(BM_ADDRESS)) {
+        debugOutput("Total battery capacity: " + String(batteryData.totalCapacity) + " kWh", 4, true);
+    } else {
+        debugOutput("Error retreiving total battery capacity", 2, true);
+    }
 
     esp_efuse_mac_get_default(chipid);
     for (i = 0; i < 6; i++)
@@ -186,8 +190,7 @@ void setup()
         "0.8.0.0", // Manufacturer's Software version code
         "1.0.0.0", // Manufacturer's Model version
         N2K_LOAD_LEVEL, // Device power load on N2k bus
-        0xffff, 0x01
-    );
+        0xffff, 0x01);
     // Set device information
     NMEA2000.SetDeviceInformation(id, // Unique number. Use e.g. Serial number.
         170, // Device function=Battery:reports battery status. See codes on http://www.nmea.org/Assets/20120726%20nmea%202000%20class%20&%20function%20codes%20v%202.00.pdf
@@ -203,14 +206,17 @@ void setup()
     preferences.begin("nvs", false); // Open nonvolatile storage (nvs)
     NodeAddress = preferences.getInt("LastNodeAddress", 30); // Read stored last NodeAddress, default 30
     preferences.end();
-    debugOutput("N2k node address = " + String(NodeAddress), 4);
+    debugOutput("N2k node address = " + String(NodeAddress), 4, true);
 
     // If you also want to see all traffic on the bus use N2km_ListenAndNode instead of N2km_NodeOnly below
     NMEA2000.SetMode(tNMEA2000::N2km_NodeOnly, NodeAddress);
     NMEA2000.ExtendTransmitMessages(TransmitMessages);
 
-    debugOutput("Opening N2k stream.", 5);
-    NMEA2000.Open();
+    if (NMEA2000.Open()) {
+        debugOutput("Opened N2k stream.", 5, true);
+    } else {
+        debugOutput("Error opening N2k stream.", 5, true);
+    };
     delay(200);
 
 #ifdef WEBLOG
@@ -231,11 +237,6 @@ void setup()
     server.begin();
     debugOutput("Finished startup.", 6, true);
 #endif
-
-    //  Reduce CPU frequency to save power
-    //  debugOutput("CPU Freq: " + String(getCpuFrequencyMhz()), 7);
-    //  setCpuFrequencyMhz(80);
-    //  debugOutput("New CPU Freq: " + String(getCpuFrequencyMhz()), 7);
 }
 
 //
@@ -250,7 +251,7 @@ void loop()
     //  server.handleClient();
 #endif
 
-    debugOutput("\n\n_____________ Next loop _____________\n", 5);
+    debugOutput("_____________ Next loop _____________\n\n", 5);
 
     if (BMGetBatteryState(BM_ADDRESS)) {
         SendN2kBatteryState();
@@ -285,7 +286,7 @@ bool BMGetBatteryState(const int BMaddress)
     if (BMparseData(BMDataSentence, BM_RMSG_CMD)) {
         return true;
     } else {
-        debugOutput("No data received, wrong data type or wrong checksum!", 2);
+        debugOutput("Error parsing data sentence!", 2);
         return false;
     }
 }
@@ -304,12 +305,12 @@ bool BMGetBatterySettings(const int BMaddress)
     if (BMparseData(BMDataSentence, BM_RSETT_CMD)) {
         return true;
     } else {
-        debugOutput("No data received, wrong data type or wrong checksum!", 2);
+        debugOutput("Error parsing data sentence!", 2);
         return false;
     }
 }
 
-bool BMSendCmd(const char *c_MsgCmd, const int BMaddress)
+bool BMSendCmd(const char* c_MsgCmd, const int BMaddress)
 {
     const char cmdHead[] = ":";
     const char Equals[] = "=";
@@ -335,10 +336,9 @@ bool BMSendCmd(const char *c_MsgCmd, const int BMaddress)
 String BMreadMsg()
 {
     char c;
-    String receivedData;
+    String receivedData = "";
 
     debugOutput("No. of bytes available on RS485: " + String(SerRS485.available()), 6);
-
     while (SerRS485.available() > 0) { // Check if data is available to read
         c = SerRS485.read(); // Read the next incoming char
 
@@ -349,34 +349,48 @@ String BMreadMsg()
         }
     }
 
+    // to be corrected "sizeof"
+    if (sizeof(receivedData) == 0) {
+        debugOutput("No data received", 2);
+    } else {
+        debugOutput("Dataset size:" + String(sizeof(receivedData)), 7);
+        debugOutput("Data: " + receivedData, 7);
+    }
+
     // SerRS485.flush(); // drop everything in buffer after <CR>
     while (SerRS485.available()) { // drop everything in buffer after <CR> (<Serial.flush()> does not seem to work properly)
         SerRS485.read();
     }
 
-    debugOutput("Data: " + receivedData, 7);
     return receivedData;
 }
 
-bool BMparseData(const String data, const char *MsgCmd)
+bool BMparseData(const String data, const char* MsgCmd)
 {
-    // result syntax: :r50=<addr>,<checksum>,<voltage>,<current_amps>,<remaining_batt_cap>,<discharged_kWh>,<charged_kWh>,<op_record_val>,
-    // <temp>,<reserved>,<output_state>,<current_direction>,<remaining_time>,<time_adj>,<date>,<time>
+    // KH-F data format:
+    // * Measures - :r50=<addr>,<checksum>,<voltage>,<current_amps>,<remaining_batt_cap>,<discharged_kWh>,<charged_kWh>,<op_record_val>,
+    //                   <temp>,<reserved>,<output_state>,<current_direction>,<remaining_time>,<time_adj>,<date>,<time>
+    // * State -    :r51=<addr>,<checksum>,<overvolt_prot>,<undervolt_prot>,<overdischrg_prot>,<overchrg_prot>,<overpwr_prot>,<overtmp_prot>,<prot_rcvr_time>,
+    //                   <prot_delay_time>,<batt_cap>,<volt_clbr>,<curr_clbr>,<temp_clbr>,<undef_func>,<relay_type>,<curr_mltplr>,<time_clbr>,<logging>,
+    //                   <full_chrg_volt>,<low_batt_volt>,<full_chrg_curr>,<monitor_time>,<low_temp_prot>,<temp_unit>,<bluet_pwd>,datalog_interval>
 
     char c_MsgNr[3] = ""; // pure message command number w/o leading character
+    String c_MsgCmd;
     int i, dataSetNo;
     long dataSet[BM_MAX_MSG_NO];
     int startIdx = 0;
     int nextIdx = 0;
 
-    sprintf(c_MsgNr, "%*s", 2, MsgCmd+1);
-//    c_MsgCmd[0] = toupper((unsigned char) MsgCmd[1]);
-    debugOutput("Expected Cmd: <" + String(c_MsgNr) + ">", 6);
-    debugOutput("CompareTo: <" + data.substring(2, 4) + ">", 6);
+    //    sprintf(c_MsgNr, "%*s", 2, MsgCmd + 1);
+    c_MsgCmd = data.substring(1, 4);
+    c_MsgCmd[0] = toupper((unsigned char)c_MsgCmd[0]);
+    debugOutput("Expected Cmd: <" + String(MsgCmd) + ">", 6);
+    debugOutput("CompareTo: <" + String(c_MsgCmd) + ">", 6);
 
-    if (data.substring(2, 4) == c_MsgNr) { // data sentence is of correct type
+    if (c_MsgCmd == MsgCmd) { // data sentence is of correct type
+        // separate data sentence into individual data fields
         i = -1;
-        startIdx = data.indexOf(','); // search for 1st data field
+        startIdx = data.indexOf(','); // search for 1st data field after command + device address -> checksum
         nextIdx = data.indexOf(',', startIdx + 1); // search for end of current data field
         while (nextIdx != -1 && i < BM_MAX_MSG_NO) {
             i++;
@@ -388,11 +402,24 @@ bool BMparseData(const String data, const char *MsgCmd)
         }
         dataSetNo = i; // store no. of fields in parsed data
 
-    } else { // data sentence is empty or of wrong type
+    } else { // data sentence is of wrong type
+        debugOutput("Data sentence is of wrong message type.", 2);
         return false;
     }
 
-    if (BMchkChecksum(dataSet, dataSetNo)) { // if checksum of data sentence is correct
+    // verify checksum of data sentence
+    if (!BMchkChecksum(dataSet, dataSetNo)) {
+        debugOutput("Data sentence has wrong check sum.", 2);
+        return false;
+    }
+
+    if (c_MsgCmd == BM_RSETT_CMD) { // data sentence contains battery settings values
+
+        batteryData.totalCapacity = float(dataSet[9]) / 10;
+        debugOutput("Total capacity: " + String(batteryData.totalCapacity) + " kWh", 5);
+
+    } else if (c_MsgCmd == BM_RMSG_CMD) { // data sentence contains battery measure values
+
         batteryData.checksum = int(dataSet[0]); // conversion of double to int, since "String.toInt()" returns "long"
         batteryData.voltage = float(dataSet[1]) / 100;
         batteryData.current = float(dataSet[2]) / 100;
@@ -408,16 +435,12 @@ bool BMparseData(const String data, const char *MsgCmd)
         batteryData.timeAdjust = int(dataSet[12]);
         batteryData.date = dataSet[13];
         batteryData.lastReadTime = dataSet[14];
-
         batteryData.stateOfCharge = int(batteryData.remainingCapacity / batteryData.totalCapacity * 100);
 
         debugOutput("Checksum: " + String(batteryData.checksum), 5);
         debugOutput("Voltage: " + String(batteryData.voltage) + " V", 5);
         debugOutput("Current: " + String(batteryData.current) + " A", 5);
         debugOutput("Remaining capacity: " + String(batteryData.remainingCapacity) + " kWh", 5);
-        debugOutput("Total capacity: " + String(batteryData.totalCapacity) + " kWh", 5);
-        debugOutput("State of charge SOC: " + String(batteryData.stateOfCharge) + " %", 5);
-        debugOutput("State of health SOH: " + String(batteryData.stateOfHealth) + " %", 5);
         debugOutput("Discharged kWh: " + String(batteryData.dischargedKWh) + " kWh", 5);
         debugOutput("Charged kWh: " + String(batteryData.chargedKWh) + " kWh", 5);
         debugOutput("Oper value: " + String(batteryData.operationRecVal), 5);
@@ -428,10 +451,10 @@ bool BMparseData(const String data, const char *MsgCmd)
         debugOutput("Remaining batt time: " + String(batteryData.batteryTimeLeft) + " min.", 5);
         debugOutput("Time adjust: " + String(batteryData.timeAdjust), 5);
         debugOutput("Date: " + String(batteryData.date), 5);
-        debugOutput("Time: " + String(batteryData.lastReadTime), 5);
+        debugOutput("Time: " + String(batteryData.lastReadTime) + "\n", 5);
 
-    } else { // checksum of data sentence is wrong
-        return false;
+        debugOutput("State of charge SOC: " + String(batteryData.stateOfCharge) + " %", 5);
+        debugOutput("State of health SOH: " + String(batteryData.stateOfHealth) + " %", 5);
     }
 
     return true;
@@ -485,8 +508,6 @@ void SendN2kBatteryState(void)
         unsigned char battSOC = (unsigned char)(batteryData.stateOfCharge);
         unsigned char battSOH = batteryData.stateOfHealth;
         double battTimeLeft = double(batteryData.batteryTimeLeft * 60); // remaining battery time is expected to be in seconds for N2k PGN 127506
-        debugOutput("Battery time left long: " + String(batteryData.batteryTimeLeft), 6);
-        debugOutput("Battery time left double: " + String(battTimeLeft), 6);
         double battTotalCap = AhToCoulomb(double(batteryData.totalCapacity));
 
         // bool ParseN2kPGN127506(const tN2kMsg &N2kMsg, unsigned char &SID, unsigned char &DCInstance, tN2kDCType &DCType,
@@ -557,44 +578,47 @@ void debugOutput(String text)
     debugOutput(text, 4); // no loglevel present? use "INFO"
 }
 
-/*
-  void ensureWIFIConnection() {
-  if (WiFi.status() != WL_CONNECTED) {
-    debugOutput("No WIFI Connection found. Re-establishing...", 3, true);
-    int connectionRetry = 0;
-    while ((wifiMulti.run(CONNECTION_TIMEOUT_MS) != WL_CONNECTED)) {
-      delay(1000);
-      connectionRetry++;
-      debugOutput("WLAN Connection attempt number " + String(connectionRetry), 4, true);
-      if (connectionRetry > MAX_CONNECTION_RETRY) {
-        debugOutput("Connection Failed! Rebooting...", 4, true);
-        delay(5000);
-        ESP.restart();
-      }
+// #ifdef WEBLOG
+void ensureWIFIConnection()
+{
+    if (WiFi.status() != WL_CONNECTED) {
+        debugOutput("No WIFI Connection found. Re-establishing...", 3, true);
+        int connectionRetry = 0;
+        while ((wifiMulti.run(CONNECTION_TIMEOUT_MS) != WL_CONNECTED)) {
+            delay(1000);
+            connectionRetry++;
+            debugOutput("WLAN Connection attempt number " + String(connectionRetry), 4, true);
+            if (connectionRetry > MAX_CONNECTION_RETRY) {
+                debugOutput("Connection Failed! Rebooting...", 4, true);
+                delay(5000);
+                ESP.restart();
+            }
+        }
+        debugOutput("WiFi is connected", 4, true);
+        debugOutput("IP address: " + (WiFi.localIP().toString()), 4, true);
+        debugOutput("Connected to (SSID): " + String(WiFi.SSID()), 5, true);
+        debugOutput("Signal strength (RSSI): " + String(WiFi.RSSI()) + "(-50 = perfect / -100 no signal)", 5, true);
     }
-    debugOutput("WiFi is connected", 4, true);
-    debugOutput("IP address: " + (WiFi.localIP().toString()), 4, true);
-    debugOutput("Connected to (SSID): " + String(WiFi.SSID()), 5, true);
-    debugOutput("Signal strength (RSSI): " + String(WiFi.RSSI()) + "(-50 = perfect / -100 no signal)", 5, true);
-  }
-  }
+}
 
-  String renderHtml(String header, String body) {
-  // HTML & CSS contents which display on web server
-  String html = "";
-  html = html + "<!DOCTYPE html>\n<html>\n" + "<html lang='de'>\n<head>\n<meta charset='utf-8'>\n<title>" + header + "</title>\n</head><body>\n<h1>";
-  html = html + header + "</h1>\n";
-  html = html + body + "\n</body>\n</html>\n";
-  return html;
-  }
+String renderHtml(String header, String body)
+{
+    // HTML & CSS contents which display on web server
+    String html = "";
+    html = html + "<!DOCTYPE html>\n<html>\n" + "<html lang='de'>\n<head>\n<meta charset='utf-8'>\n<title>" + header + "</title>\n</head><body>\n<h1>";
+    html = html + header + "</h1>\n";
+    html = html + body + "\n</body>\n</html>\n";
+    return html;
+}
 
-  void respondRequestWithLogEntries() {
-  String header = "Debugging-Log-Entries";
-  String body = "";
-  body = "<h2>Logging on Startup / during configuration (Setup-Log)</h2>\n";
-  body = body + setupLogText;
-  body = body + "<h2>Logging during operation (Loop-Log)</h2>\n";
-  body = body + loopLogText;
-  server.send(200, "text/html; charset=utf-8", renderHtml(header, body));
-  }
-*/
+void respondRequestWithLogEntries()
+{
+    String header = "Debugging-Log-Entries";
+    String body = "";
+    body = "<h2>Logging on Startup / during configuration (Setup-Log)</h2>\n";
+    body = body + setupLogText;
+    body = body + "<h2>Logging during operation (Loop-Log)</h2>\n";
+    body = body + loopLogText;
+    server.send(200, "text/html; charset=utf-8", renderHtml(header, body));
+}
+// #endif
